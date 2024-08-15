9b6f0477b7d806ba49c51d06bae3465ffae334f5
Eli Knebel <eliknebel@cmu.edu>
2024-08-14 22:00:39 -0400

    fix double click hook

--- client/src/hooks/doubleclick.ts
+++ client/src/hooks/doubleclick.ts
@@ -1,5 +1,7 @@
-export const doubleclick = {
-  mounted({ el, pushEvent }) {
+import { ClientHook } from "sprocket-js";
+
+export const DoubleClick: ClientHook = {
+  create({ el, pushEvent }) {
     el.addEventListener("dblclick", () => {
       pushEvent("doubleclick", {});
     });
